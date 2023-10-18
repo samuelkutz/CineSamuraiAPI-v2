a2f2c753ad781e34840477aaebdbab8597d5dd6e
samuelkutz <samuelkutzparanhos1@gmail.com>
2023-10-18 17:38:30 -0300

    feat: basic post method for user (no db connection)

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,10 +3,19 @@ class UserController{
         const { nome, email, cpf, senha, telefone } = req.body
 
         if (!nome || !email || !cpf || !senha || !telefone) {
-            res.send({ message : "Preencha todos os campos"})
+            res.status(400).send({ message : "Preencha todos os campos"})
         }
 
-        res.send("xD")
+        res.status(201).send({
+            message: "Usuário criado com sucesso",
+            user: {
+                nome,
+                email,
+                cpf,
+                senha,
+                telefone
+            }
+        })
     }
 }
 
