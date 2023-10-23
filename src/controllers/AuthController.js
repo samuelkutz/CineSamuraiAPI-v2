466011a7f43db2a381eac9b758216ea9e42f2d87
samuelkutz <samuelkutzparanhos1@gmail.com>
2023-10-23 12:04:40 -0300

    feat: implementacao inicial de jwt

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -7,16 +7,21 @@ class AuthController {
             const { email, senha } = req.body
 
             const user = await AuthService.findByEmail(email) // findByEmail returns user object with the password included
+
             if (!user) {
                 res.status(404).send({ message: "Usuário ou senha não encontrados" })
             }
 
             const isValidSenha = await bcrypt.compare(senha, user.senha)
+
             if (!isValidSenha) {
                 res.status(404).send({ message: "Usuário ou senha não encontrados" })
             }
 
-            res.send(user)
+            const token = AuthService.generateToken(user.id) //synchronous
+
+            res.send({ token: token }) 
+            
         } catch (err) {
             console.log(err)
             res.status(500).send({ message: err })
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,8 +1,13 @@
 import { User } from "../models/User.js"
+import jwt from "jsonwebtoken"
 
 class AuthService{
     static findByEmail(email){
         return User.findOne( { email: email } ).select("+senha") // user DOES NOT have the password naturally, thats why we need to modify the select method
+    }
+
+    static generateToken(id) {
+        jwt.sign({ id: id }, procces.env.SECRET_JWT, { expiresIn: 86400 }) // token expira em 24h
     }
 }
 
