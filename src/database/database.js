73d8faba301bb1a03385c8cf95ff3668efcbbb46
Lucas Rolando <lucasrls98@gmail.com>
2023-11-08 23:00:08 -0300

    refactor: minimização da url de conexao ao banco de dados

--- src/database/database.js
+++ src/database/database.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
 async function connectDatabase() {
-  await mongoose
-    .connect(process.env.DATABASE_URI, { // note that dotenv needs to be imported ONLY where connectDatabase() runs
-      useNewUrlParser: true,
-      // useUnifiedTopology: true,
-    })
+  await mongoose.connect(process.env.DATABASE_URI)
+    // .connect(process.env.DATABASE_URI, { // note that dotenv needs to be imported ONLY where connectDatabase() runs
+    //   useNewUrlParser: true,
+    //   // useUnifiedTopology: true,
+    // })
     
 }
 
